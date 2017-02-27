58320b9f699f0509819d67a46a1235eae3712687
Caroline Weinberg <weinberg.caroline@gmail.com>
2017-02-27 16:09:21 -0500

    Added more organized switch_controller code

--- scripts/switch_controller.py
+++ scripts/switch_controller.py
@@ -1,121 +1,160 @@
 #!/usr/bin/python
 
 # open a microphone in pyAudio and listen for taps
+#Obtained most source from http://stackoverflow.com/questions/4160175/detect-tap-with-pyaudio-from-live-mic
 
 import pyaudio
 import struct
 import math
+import time
 
-INITIAL_TAP_THRESHOLD = 0.010
-FORMAT = pyaudio.paInt16 
-SHORT_NORMALIZE = (1.0/32768.0)
-CHANNELS = 2
-RATE = 44100  
-INPUT_BLOCK_TIME = 0.05
-INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
-# if we get this many noisy blocks in a row, increase the threshold
-OVERSENSITIVE = 15.0/INPUT_BLOCK_TIME                    
-# if we get this many quiet blocks in a row, decrease the threshold
-UNDERSENSITIVE = 120.0/INPUT_BLOCK_TIME 
-# if the noise was longer than this many blocks, it's not a 'tap'
-MAX_TAP_BLOCKS = 0.15/INPUT_BLOCK_TIME
+class TapDetector(object):
+    TAP_THRESHOLD = 0.20
+    FORMAT = pyaudio.paInt16 
+    SHORT_NORMALIZE = (1.0/32768.0)
+    RATE = 44100  
+    INPUT_BLOCK_TIME = 0.05
+    INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)             
+    CHANNELS = 2
+    MAX_TAP_BLOCKS = 0.15/INPUT_BLOCK_TIME
 
-def get_rms( block ):
-    # RMS amplitude is defined as the square root of the 
-    # mean over time of the square of the amplitude.
-    # so we need to convert this string of bytes into 
-    # a string of 16-bit samples...
-
-    # we will get one short out for each 
-    # two chars in the string.
-    count = len(block)/2
-    format = "%dh"%(count)
-    shorts = struct.unpack( format, block )
-
-    # iterate over the block.
-    sum_squares = 0.0
-    for sample in shorts:
-        # sample is a signed short in +/- 32768. 
-        # normalize it to 1.0
-        n = sample * SHORT_NORMALIZE
-        sum_squares += n*n
-
-    return math.sqrt( sum_squares / count )
-
-class TapTester(object):
-    def __init__(self):
+    def __init__(self, communicator):
         self.pa = pyaudio.PyAudio()
         self.stream = self.open_mic_stream()
-        self.tap_threshold = INITIAL_TAP_THRESHOLD
-        self.noisycount = MAX_TAP_BLOCKS+1 
-        self.quietcount = 0 
         self.errorcount = 0
+        self.communicator = communicator
+        self.hasCooledDown = True
 
     def stop(self):
         self.stream.close()
 
-    def find_input_device(self):
-        device_index = None            
-        for i in range( self.pa.get_device_count() ):     
-            devinfo = self.pa.get_device_info_by_index(i)   
-            print( "Device %d: %s"%(i,devinfo["name"]) )
+    def tapDetected(self):
+        if self.hasCooledDown:
+            self.communicator.tapDetected()
+        self.hasCooledDown = False
 
-            for keyword in ["mic","input"]:
-                if keyword in devinfo["name"].lower():
-                    print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
-                    device_index = i
-                    return device_index
-
-        if device_index == None:
-            print( "No preferred input found; using default input device." )
-
-        return device_index
+    def noTapDetected(self):
+        self.hasCooledDown = True
+        self.communicator.noTapDetected()
 
     def open_mic_stream( self ):
-        # device_index = self.find_input_device()
-
-        stream = self.pa.open(   format = FORMAT,
-                                 channels = CHANNELS,
-                                 rate = RATE,
+        stream = self.pa.open(   format = self.FORMAT,
+                                 channels = self.CHANNELS,
+                                 rate = self.RATE,
                                  input = True,
-                                 # input_device_index = device_index,
-                                 frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+                                 frames_per_buffer = self.INPUT_FRAMES_PER_BLOCK)
 
         return stream
 
-    def tapDetected(self):
-        print "Tap!"
-
     def listen(self):
         try:
-            block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
+            block = self.stream.read(self.INPUT_FRAMES_PER_BLOCK)
         except IOError, e:
-            # dammit. 
             self.errorcount += 1
-            print( "(%d) Error recording: %s"%(self.errorcount,e) )
-            self.noisycount = 1
+            print( "(%d) Error recording: %s" % (self.errorcount,e) )
             return
 
-        amplitude = get_rms( block )
-        if amplitude > self.tap_threshold:
-            # noisy block
-            self.quietcount = 0
-            self.noisycount += 1
-            if self.noisycount > OVERSENSITIVE:
-                # turn down the sensitivity
-                self.tap_threshold *= 1.1
-        else:            
-            # quiet block.
+        amplitude = self.get_rms( block )
+        if amplitude > self.TAP_THRESHOLD:
+            self.tapDetected()
+        else:
+            self.noTapDetected()
 
-            if 1 <= self.noisycount <= MAX_TAP_BLOCKS:
-                self.tapDetected()
-            self.noisycount = 0
-            self.quietcount += 1
-            if self.quietcount > UNDERSENSITIVE:
-                self.tap_threshold *= 0.9
+    def get_rms( self, block ):
+        # RMS amplitude is defined as the square root of the 
+        # mean over time of the square of the amplitude.
+        # so we need to convert this string of bytes into 
+        # a string of 16-bit samples...
+
+        # we will get one short out for each 
+        # two chars in the string.
+        count = len(block)/2
+        format = "%dh"%(count)
+        shorts = struct.unpack( format, block )
+
+        # iterate over the block.
+        sum_squares = 0.0
+        for sample in shorts:
+            # sample is a signed short in +/- 32768. 
+            # normalize it to 1.0
+            n = sample * self.SHORT_NORMALIZE
+            sum_squares += n*n
+
+        return math.sqrt( sum_squares / count )
+
+class Communicator(object):
+    WAIT_THRESHOLD = 0.5
+    NEXT_THRESHOLD = 1
+    SELECT_THRESHOLD = 2
+
+    def __init__(self, ui):
+        self.last_tap = time.time() - 100
+        self.current_time = time.time()
+        self.states = {"select": "3\n", "next": "2\n", "none": "1\n"}
+
+        self.ui = ui
+
+    def tapDetected(self):
+        current_tap = time.time()
+        time_since_last_tap = current_tap - self.last_tap
+        if 0 <= time_since_last_tap <= self.WAIT_THRESHOLD:
+            self.initialTap()
+        elif self.WAIT_THRESHOLD < time_since_last_tap <= self.NEXT_THRESHOLD:
+            self.submitNext()
+        elif self.NEXT_THRESHOLD < time_since_last_tap <= self.SELECT_THRESHOLD:
+            self.submitSelect()
+        else: #Took too long on tap
+            self.initialTap()
+
+    def noTapDetected(self):
+        current_tap = time.time()
+        time_since_last_tap = current_tap - self.last_tap
+        if 0 <= time_since_last_tap <= self.WAIT_THRESHOLD:
+            self.ui.waitingForInput()
+        elif self.WAIT_THRESHOLD < time_since_last_tap <= self.NEXT_THRESHOLD:
+            self.ui.registeringNext()
+        elif self.NEXT_THRESHOLD < time_since_last_tap <= self.SELECT_THRESHOLD:
+            self.ui.registeringSelect()
+        else: #Waiting for tap
+            self.ui.waitingForInput()
+
+    def submitNext(self):
+        print("Submitting next")
+        self.resetLastTap()
+
+    def submitSelect(self):
+        print("Submitting select")
+        self.resetLastTap()
+
+    def initialTap(self):
+        print("Initial tap")
+        self.updateLastTap()
+
+    def updateLastTap(self):
+        self.last_tap = time.time()
+
+    def resetLastTap(self):
+        self.last_tap = time.time() - self.SELECT_THRESHOLD
+
+
+class UserInterface(object):
+    def __init__(self):
+        pass
+
+    def registeringNext(self):
+        pass
+        # print("Registering next")
+    def registeringSelect(self):
+        pass
+        # print("Registering select")
+    def waitingForInput(self):
+        pass
+        # print("Waiting for input")
 
 if __name__ == "__main__":
-    tt = TapTester()
+    ui = UserInterface()
+    communicator = Communicator(ui)
+    tt = TapDetector(communicator)
 
-    for i in range(1000):
+    while True:
         tt.listen()
